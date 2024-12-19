35411f95e2a36e2170a56ceeea97a0954d69db96
adswn <adisetiawan.7b@gmail.com>
2024-12-18 21:09:06 -0900

    18/12/24

--- JS.FOLDER/latihanPart2.js
+++ JS.FOLDER/latihanPart2.js
@@ -1,17 +1,22 @@
 var penumpang = ['raka', 'rido', 'setiawan']
 var hapusPenumpang = function(namaPenumpang, penumpang) {
         if (penumpang.length == 0) {
-            penumpang.push(namaPenumpang);
+          
             console.log('angkot kosong');
 
             return penumpang;
         } else {
-            for (var i = 0; i > penumpang.length; i--) {
-                if (penumpang [i] = undefined);
-
+            for (var i = 0; i < penumpang.length; i++) {
+                if (penumpang [i] == undefined) {
+                    
                 penumpang [i] = namaPenumpang;
 
                 return penumpang;
+                } else if (i == penumpang.length - 1) {
+                    console.timeLog(namaPenumpang + 'tidak ada orang di angkot');
+                }
+
+                
             }
         }
 }
