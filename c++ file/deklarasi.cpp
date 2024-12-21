066fc6506796c8f735b29554ddf5fd7961effcff
adswn <adisetiawan.7b@gmail.com>
2024-12-20 20:34:11 -0900

    c++ kembali

---
+++ c++ file/deklarasi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    a = 80;
+    cout << a << endl;
+
+    int b;
+    cout << "masukan nilai : ";
+    cin >> b;
+    cout << "nilai yang adana masukan :";
+    cout << b << endl;
+   
+    cin.get();
+    return 0;
+}
---
+++ c++ file/printing.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std:: cout << "halo \n";
+    std:: cout << "dia" << std :: endl;
+    std ::cin.get();
+    return 0;
+}
