40ffab60c370927a60c7b1fb26d17a7c15141e01
adswn <adisetiawan.7b@gmail.com>
2024-12-23 21:19:19 -0900

    23/12/24

--- JS.FOLDER/object.js
+++ JS.FOLDER/object.js
@@ -1,3 +1,4 @@
+//LITERAL
 /*var nilaiSMA = {};
     var Nama = "abdul";
     var kelas = 2;
@@ -14,3 +15,28 @@ var mhs = {
     umur : 80,
     ips : [3.5, 4.00, 3.00]
 };
+
+//FUNCTION DEKLARASI
+
+function buatObjectnamasiswa(nama, kelas, no, sekolah) {
+    var namasiswa = {};
+    namasiswa.nama = nama;
+    namasiswa.kelas = kelas;
+    namasiswa.no = no;
+    namasiswa.sekolah = sekolah;
+
+    return namasiswa;
+} 
+
+var iden = buatObjectnamasiswa("dody", "11", "11", "smansa")
+
+//CONSTRUCTOR
+
+function dataDiri(nama, umur, status, jender) {
+    this.nama = nama;
+    this.umur = umur;
+    this.status = status;
+    this.jender = jender;
+}
+
+var from = new dataDiri("roni", 17, "jomblo", "laki-laki")
