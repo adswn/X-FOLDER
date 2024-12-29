9fe2e81c74612a46fc44a48bfbc4aa41fcebe5ee
adswn <adisetiawan.7b@gmail.com>
2024-12-28 20:59:44 -0900

    28/12/24

---
+++ c++ file/assignment.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 10;
+
+    cout << "nilai awal a adalah = :" << a << endl;
+
+    // operator assignment 
+
+    a += 7;
+    cout << "ditambah menjadi :" << a << endl;
+    a -= 7;
+    cout << "dikurang menjadi :" << a << endl;
+    a *= 7;
+    cout << "dikali menjadi :" << a << endl;
+    a /= 7;
+    cout << "dibagi menjadi :" << a << endl;
+    a %= 3;
+    cout << "dimodulus menjadi :" << endl;
+
+     cin.get();
+     return 0;
+  
+
+}
+
+
