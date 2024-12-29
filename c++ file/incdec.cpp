fab49beffc9e59298e9a713d15f1d9714506c873
adswn <adisetiawan.7b@gmail.com>
2024-12-28 21:22:14 -0900

    incdec 28/12/24

---
+++ c++ file/incdec.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // inermeant and decremeant
+    
+    int a = 6;
+    int b = 6;
+
+    // postincreant
+    cout << a << endl;
+    cout << a++ << endl;
+    cout << a << endl << endl;
+
+    //preincreant
+    cout << b << endl;
+   cout << ++b << endl;
+    cout << b << endl;
+
+
+    cin.get();
+    return 0;
+}
+
