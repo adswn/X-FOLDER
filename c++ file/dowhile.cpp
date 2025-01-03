87341747cda2606e30f31bad8d35fe433bfec297
adswn <adisetiawan.7b@gmail.com>
2025-01-02 21:21:33 -0900

    2/1/25

---
+++ c++ file/dowhile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int n = 1;
+
+  do{
+    cout << "do ";
+    cout << n << endl;
+    n++;
+  } while(n <= 10);
+   
+ cout << "selesai" << endl;
+    cin.get();
+    return 0;
+}
