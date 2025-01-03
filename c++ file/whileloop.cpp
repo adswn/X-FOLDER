19c6f10991c6152da4897389d134b884cdfcb7bd
adswn <adisetiawan.7b@gmail.com>
2025-01-02 20:52:09 -0900

    29/12/24 (new)

--- c++ file/whileloop.cpp
+++ c++ file/whileloop.cpp
@@ -5,12 +5,13 @@ int main() {
 
     int a = 5;
 
-    while(a <= 10) {
-        cout << "loop";
+    while(a < 10) {
+        cout << "loop ";
         cout << a << endl;
         a += 2;
-        cout << "selesai" << endl;
     }
+
+    cout << "selesai" << endl;
 
     cin.get();
     return 0;
