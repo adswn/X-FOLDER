d5f3013e53420ecb706a8f968ae4249c751bc138
adswn <adisetiawan.7b@gmail.com>
2025-01-03 21:06:59 -0900

    3/1/25

---
+++ c++ file/forloop.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "loop1 \n";
+    for (int i = 1; i <= 10; i++){
+        cout << i << endl;
+    }
+
+      cout << "loop2 \n";
+    for (int i = 1; i >= -10; i--){
+        cout << i << endl;
+    }
+
+      cout << "loop3 \n";
+    for (int i = 1; i <= 10; i*= 3){
+        cout << i << endl;
+    }
+
+      cout << "loop4 \n";
+
+      int total = 0;
+    for (int i = 1; i <= 10; i++,  total += i){
+     
+        cout << i << "||" << total << endl;
+    }
+
+
+    cin.get();
+    return 0;
+}
