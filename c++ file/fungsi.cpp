280f69e104fc26a07763764f41e9ddc8c0898a06
adswn <adisetiawan.7b@gmail.com>
2025-01-17 20:47:37 -0900

    dadu

--- c++ file/fungsi.cpp
+++ c++ file/fungsi.cpp
@@ -7,11 +7,17 @@ int main(){
 
     int x;
     int y;
+    
 
     cout << "masukan fungsi x: ";
     cin >> x;
 
-     double a = ceil(x);
+    cout << "masukan fungsi y: "; 
+    cin >> y;
+
+    
+
+     char a = ceil(x);
     cout << "pembulatan ke atas x = " << a << endl;
 
      double b = cos(x);
@@ -23,11 +29,12 @@ int main(){
      double d = fabs(x);
     cout << "nilai absolut float x = " << d << endl;
 
-     double e = floor(x);
+     double2
+      e = floor(x);
     cout << "pembulatan ke bawah x = " << e << endl;
 
-     float f = fmod(x);
-    cout << "modulus dalam float x = " << f << endl;
+     float f = fmod(x, y);
+    cout << "modulus dalam float x dan y = " << f << endl;
 
      double g = log(x);
     cout << "logaritma basis natural x = " << g << endl;
@@ -35,9 +42,8 @@ int main(){
      double h = log10(x);
     cout << "logaritma basis 10 x = " << h << endl;
 
-     double i = pow(x, y);
-
-     cout << "pangkat x = " << i << endl;
+    double i = pow(x, y);
+    cout << "pangkat x = " << i << endl;
 
     double j = sin(x);
     cout << "sinus x = " << j << endl;
