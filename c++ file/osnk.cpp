7ab364559e3f7fd662e7470fc67b7ce29a896a9c
adswn <adisetiawan.7b@gmail.com>
2025-01-17 21:15:49 -0900

    pengembalian

--- c++ file/osnk.cpp
+++ c++ file/osnk.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-bool dua_mata(vector<int> A, int kiri, int kanan) {
-    if (kiri > kanan) {
-        return false; // Base case when the range is invalid
-    }
-    int mid = (kiri + kanan) / 2;
-    if (A[mid] == 0) {
-        return true; // Found zero
-    } else if (A[mid] < 0) {
-        return dua_mata(A, mid + 1, kanan); // Search in the right half
-    } else {
-        return dua_mata(A, kiri, mid - 1); // Search in the left half
+
+bool dua_mata(vektor<int> A, int kiri, int kanan) {
+    if (kiri == kanan){
+        return(A[kiri] == 0);
+    }else{
+        int mid = (kiri + kanan)/2;
+        if (A[mid] < 0){
+            return dua_mata(A,kiri, mid-1);
+        }else if (A[mid] > 0) {
+            return dua_mata(A,mid+1,kanan);
+        } else{
+            return false;
+        }
     }
 }
-
-
-
-
-
-
+    
+    
 
