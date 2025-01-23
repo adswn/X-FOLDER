2d08c9e0c08419c9fd42ebd01b1b4b19379e8a3e
adswn <adisetiawan.7b@gmail.com>
2025-01-22 20:25:26 -0900

    tranversal

---
+++ JS.FOLDER/tranversal.js
@@ -0,0 +1,28 @@
+/*const close = document.querySelector('.close');
+const card = document.querySelector('.card');
+
+close.addEventListener('click',function(){
+    card.style.display = 'none';
+});*/
+
+const close = document.querySelectorAll('.close');
+
+
+/*for(let i = 0; i < close.length; i++){
+   close[i].addEventListener('click',function(e){
+       //close[i].parentElement.style.display = 'none';
+         e.target.parentElement.style.display = 'none';
+
+   });
+}*/
+close.forEach(function(el){
+    el.addEventListener('click',function(e){
+        e.target.parentElement.style.display = 'none';
+        
+    });
+    
+});
+
+//parentNode//parentElement
+//nextSibling//nextElementSibling
+//previousSibling//previousElementSibling
