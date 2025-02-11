8189087b91be2f321f47a80000bb61ecaa9f1a96
adswn <adisetiawan.7b@gmail.com>
2025-02-10 21:06:55 -0900

    tranvelsal new

--- JS.FOLDER/tranvelsal.js
+++ JS.FOLDER/tranvelsal.js
@@ -5,7 +5,7 @@ close.addEventListener('click', function(){
     card.style.display = 'none';
 });*/
 
-const close = document.querySelectorAll('.close');
+//const close = document.querySelectorAll('.close');
 
 /*for(let i = 0; i < close.length; i++){
     close[i].addEventListener('click', function(e){
@@ -23,11 +23,27 @@ const close = document.querySelectorAll('.close');
     });
 });*/
 
-close.forEach(function(el) {
+/*close.forEach(function(el) {
     el.addEventListener('click', function(e) {
         e.target.parentElement.style.display = 'none';
         e.preventDefault();
+        e.stopPropagation();
     });
+});
+
+const cards = document.querySelectorAll('.card');
+cards.forEach(function(card){
+    card.addEventListener('click', function(e){
+        alert('nyp wong');
+    });
+});*/
+
+const container = document.querySelector('.container');
+container.addEventListener('click', function(e){
+    if(e.target.classname == 'close'){
+        e.target.parentElement.style.display = 'none';
+        e.preventDefault();
+    }
 });
 
 //parentNode//parentElement
