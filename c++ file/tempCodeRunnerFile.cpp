9e76cac3dd2e801f903957256a21c0eef9b4b6cc
adswn <adisetiawan.7b@gmail.com>
2025-02-15 21:22:08 -0900

    latihan array

--- c++ file/tempCodeRunnerFile.cpp
+++ c++ file/tempCodeRunnerFile.cpp
@@ -1,16 +1,2 @@
-#incude <iostream>
-using namespace std;
 
-int main() {
-
-
-bool cek(string S)} {
-int n = S.length();
-cek("BEGITU");
-for (int i = 0; i < n-1; i++) {
- if (S[i] > S[i+1]) return false;
-}
-return true;
-}
-
-
+    return 0;
