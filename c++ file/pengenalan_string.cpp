0365fcdf75ac22e6b4a659c0dd8e94b7bdb06039
adswn <adisetiawan.7b@gmail.com>
2025-02-27 21:39:14 -0900

    pengenalan string

---
+++ c++ file/pengenalan_string.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    // array char tidak bisa kita tambah, fiks karena array
+
+
+    string kata ("spm group");
+    cout << kata << endl;
+
+    string date;
+    cout << "masukan kata / kalimat : " << endl;
+    cin >> date;
+    cout << "data yang anda masukan : " << date << endl;
+    //cout << date << endl;
+
+
+
+    cin.get();
+    return 0;
+}
