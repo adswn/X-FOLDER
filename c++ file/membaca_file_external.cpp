e592d7c805b3ea6978931a6c7aa6e86463b5f60e
adswn <adisetiawan.7b@gmail.com>
2025-03-14 20:56:20 +0700

    menulis binary

--- c++ file/membaca_file_external.cpp
+++ c++ file/membaca_file_external.cpp
@@ -1,35 +1,12 @@
 #include <iostream>
-#include <fstream> //ofstream,ifstream,fstream
+#include <fstream>
 #include <string>
 using namespace std;
 
 int main(){
-    ifstream myfile;
-    string output, buffer;
-    bool isdata = false;
-    int no;
-    string nama;
   
-    myfile.open("data.txt", ios::in);
-    while(!isdata){
-        getline(myfile, buffer);
-       output.append("\n" + buffer);
-       if(buffer == "data"){
-            isdata = true;
-            
-       }
-        cout << output << endl;
-    }getline(myfile,buffer);
-	cout << buffer << endl;
-	int jumlah_data = 0;
-	while(!myfile.eof()){
-		myfile >> no;
-		myfile >> nama;
 
-		cout << no << "\t" << nama << endl;
-		jumlah_data++;
-	}
-	cout << "jumlah data = " << jumlah_data << endl;
+   
 
 
     cin.get();
