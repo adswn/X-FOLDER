a0ef75936a68b3341c5cf0535fc0c44e7d6e10d1
adswn <adisetiawan.7b@gmail.com>
2025-03-14 21:20:54 +0700

    menulis binary

--- c++ file/membaca_file_external.cpp
+++ c++ file/membaca_file_external.cpp
@@ -4,7 +4,13 @@
 using namespace std;
 
 int main(){
-  
+  ifstream Fileku("dataku.file");
+  string output, buffer;
+  string nama;
+
+  Fileku.open("dataku.file");
+  cout << "berhasil" << endl;
+  Fileku.close();
 
    
 
--- c++ file/menulis_binary.cpp
+++ c++ file/menulis_binary.cpp
@@ -5,13 +5,20 @@
 using namespace std;
 
 int main(){
-    fstream fileku;
-    int number = 1;
+    //fstream fileku;
+    int number = 769804;
 
-    fileku.open("data.bin", ios::out | ios::binary);
+    /*fileku.open("data.bin", ios::out | ios::binary);
     fileku.write(reinterpret_cast<char*>(&number), sizeof(number));
     cout << "penulisan file berhasil" << endl;
-    fileku.close();
+    fileku.close();*/
+
+    fstream fileku;
+    int hasil;
+    fileku.open("data.bin", ios::in | ios::binary);
+    fileku.read(reinterpret_cast<char*>(&hasil), sizeof(hasil));
+    cout << "besar integer : " << sizeof(hasil) << endl;
+    cout << hasil << endl;
 
     cin.get();
     return 0;
